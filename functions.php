1ffafe6aac258a2c9288379507982013b9169941
haroldparis <harold.paris@tribeleadr.com>
2013-02-16 22:50:13 +0100

    added Google Analytics to the function.php

--- functions.php
+++ functions.php
@@ -644,4 +644,37 @@ function origines_home_head() {
 	endif;
 }
 
+/**
+ * Function Name: google_analytics_tracking_code
+ * Description: Add Google Analytics Tracking Code to the head or the footer of your blog.
+ */
+function google_analytics_tracking_code(){
+
+	$propertyID = 'UA-XXX'; // GA Property ID
+
+	if ($options['ga_enable']) { ?>
+		
+		<script type="text/javascript">
+
+  		var _gaq = _gaq || [];
+  		_gaq.push(['_setAccount', '<?php echo $propertyID; ?>']);
+  		_gaq.push(['_trackPageview']);
+
+  		(function() {
+    	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  		})();
+
+		</script>
+
+<?php }
+}
+
+// include GA tracking code before the closing head tag
+// add_action('wp_head', 'google_analytics_tracking_code');
+
+// OR include GA tracking code before the closing body tag
+add_action('wp_footer', 'google_analytics_tracking_code');
+
 ?>
