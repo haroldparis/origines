c2108ebc163016514bc758b4aaef0d5bd5caaf1b
haroldparis <harold.paris@tribeleadr.com>
2013-02-16 16:02:32 +0100

    wordpress admin bar now appears

--- footer.php
+++ footer.php
@@ -50,5 +50,7 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/twitter-bootstrap-hover-dropdown.min.js"></script>
 
+<?php wp_footer(); ?>
+
 </body>
 </html>
